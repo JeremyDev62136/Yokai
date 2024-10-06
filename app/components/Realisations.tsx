8e0a1888d7a6910d16f0e52528fde44ccbd505d0
JeremyDev62136 <jeremylefevre.dev@outlook.fr>
2024-10-06 15:57:42 +0200

    delete pictures

--- app/components/Realisations.tsx
+++ app/components/Realisations.tsx
@@ -1,37 +1,10 @@
 import Image from "next/image"
 import book1 from "../images/book1.png"
-import book2 from "../images/book2.png"
-import book3 from "../images/book3.png"
-import book4 from "../images/book4.png"
-import book5 from "../images/book5.png"
-import book6 from "../images/book6.png"
-import book7 from "../images/book7.png"
-import book8 from "../images/book8.png"
-import book9 from "../images/book9.png"
-import book10 from "../images/book10.png"
-import book11 from "../images/book11.png"
-import book12 from "../images/book12.png"
-import book13 from "../images/book13.png"
 import book14 from "../images/book14.png"
-import book15 from "../images/book15.png"
-import book16 from "../images/book16.png"
-import book17 from "../images/book17.png"
-import book18 from "../images/book18.png"
 import book19 from "../images/book19.png"
-import book20 from "../images/book20.png"
-import book21 from "../images/book21.png"
-import book22 from "../images/book22.png"
-import book23 from "../images/book23.png"
-import book24 from "../images/book24.png"
-import book25 from "../images/book25.png"
-import book26 from "../images/book26.png"
-import book27 from "../images/book27.png"
-import book28 from "../images/book28.png"
-import book29 from "../images/book29.png"
-import book30 from "../images/book30.png"
-import book31 from "../images/book31.png"
-import book32 from "../images/book32.png"
-import book33 from "../images/book33.png"
+import book3 from "../images/book3.png"
+import book6 from "../images/book6.png"
+import book9 from "../images/book9.png"
 
 
 export default function Realisations() {
@@ -43,19 +16,7 @@ export default function Realisations() {
                     width={3307} height={4677}
                     className="w-32 "
                     alt="photo d'un tatouage réalisé par littlekowicz" />
-                <Image src={book2}
-                    width={3307} height={4677}
-                    className="w-32 "
-                    alt="photo d'un tatouage réalisé par littlekowicz" />
                 <Image src={book3}
-                    width={3307} height={4677}
-                    className="w-32 "
-                    alt="photo d'un tatouage réalisé par littlekowicz" />
-                <Image src={book4}
-                    width={3307} height={4677}
-                    className="w-32 "
-                    alt="photo d'un tatouage réalisé par littlekowicz" />
-                <Image src={book5}
                     width={3307} height={4677}
                     className="w-32 "
                     alt="photo d'un tatouage réalisé par littlekowicz" />
@@ -63,31 +24,7 @@ export default function Realisations() {
                     width={3307} height={4677}
                     className="w-32 "
                     alt="photo d'un tatouage réalisé par littlekowicz" />
-                <Image src={book7}
-                    width={3307} height={4677}
-                    className="w-32 "
-                    alt="photo d'un tatouage réalisé par littlekowicz" />
-                <Image src={book8}
-                    width={3307} height={4677}
-                    className="w-32 "
-                    alt="photo d'un tatouage réalisé par littlekowicz" />
                 <Image src={book9}
-                    width={3307} height={4677}
-                    className="w-32 "
-                    alt="photo d'un tatouage réalisé par littlekowicz" />
-                <Image src={book10}
-                    width={3307} height={4677}
-                    className="w-32 "
-                    alt="photo d'un tatouage réalisé par littlekowicz" />
-                <Image src={book11}
-                    width={3307} height={4677}
-                    className="w-32 "
-                    alt="photo d'un tatouage réalisé par littlekowicz" />
-                <Image src={book12}
-                    width={3307} height={4677}
-                    className="w-32 "
-                    alt="photo d'un tatouage réalisé par littlekowicz" />
-                <Image src={book13}
                     width={3307} height={4677}
                     className="w-32 "
                     alt="photo d'un tatouage réalisé par littlekowicz" />
@@ -95,82 +32,10 @@ export default function Realisations() {
                     width={3307} height={4677}
                     className="w-32 "
                     alt="photo d'un tatouage réalisé par littlekowicz" />
-                <Image src={book15}
-                    width={3307} height={4677}
-                    className="w-32 "
-                    alt="photo d'un tatouage réalisé par littlekowicz" />
-                <Image src={book16}
-                    width={3307} height={4677}
-                    className="w-32 "
-                    alt="photo d'un tatouage réalisé par littlekowicz" />
-                <Image src={book17}
-                    width={3307} height={4677}
-                    className="w-32 "
-                    alt="photo d'un tatouage réalisé par littlekowicz" />
-                <Image src={book18}
-                    width={3307} height={4677}
-                    className="w-32 "
-                    alt="photo d'un tatouage réalisé par littlekowicz" />
                 <Image src={book19}
                     width={3307} height={4677}
                     className="w-32 "
                     alt="photo d'un tatouage réalisé par littlekowicz" />
-                <Image src={book20}
-                    width={3307} height={4677}
-                    className="w-32 "
-                    alt="photo d'un tatouage réalisé par littlekowicz" />
-                <Image src={book21}
-                    width={3307} height={4677}
-                    className="w-32 "
-                    alt="photo d'un tatouage réalisé par littlekowicz" />
-                <Image src={book22}
-                    width={3307} height={4677}
-                    className="w-32 "
-                    alt="photo d'un tatouage réalisé par littlekowicz" />
-                <Image src={book23}
-                    width={3307} height={4677}
-                    className="w-32 "
-                    alt="photo d'un tatouage réalisé par littlekowicz" />
-                <Image src={book24}
-                    width={3307} height={4677}
-                    className="w-32 "
-                    alt="photo d'un tatouage réalisé par littlekowicz" />
-                <Image src={book25}
-                    width={3307} height={4677}
-                    className="w-32 "
-                    alt="photo d'un tatouage réalisé par littlekowicz" />
-                <Image src={book26}
-                    width={3307} height={4677}
-                    className="w-32 "
-                    alt="photo d'un tatouage réalisé par littlekowicz" />
-                <Image src={book27}
-                    width={3307} height={4677}
-                    className="w-32 "
-                    alt="photo d'un tatouage réalisé par littlekowicz" />
-                <Image src={book28}
-                    width={3307} height={4677}
-                    className="w-32 "
-                    alt="photo d'un tatouage réalisé par littlekowicz" />
-                <Image src={book29}
-                    width={3307} height={4677}
-                    className="w-32 "
-                    alt="photo d'un tatouage réalisé par littlekowicz" />
-                <Image src={book30}
-                    width={3307} height={4677}
-                    className="w-32 "
-                    alt="photo d'un tatouage réalisé par littlekowicz" />
-                <Image src={book31}
-                    width={3307} height={4677}
-                    className="w-32 "
-                    alt="photo d'un tatouage réalisé par littlekowicz" />
-                <Image src={book32}
-                    width={3307} height={4677}
-                    className="w-32 "
-                    alt="photo d'un tatouage réalisé par littlekowicz" />
-                {/* <Image src={book33}
-                    width={3307} height={4677}
-                    className="w-32"
-                    alt="photo d'un tatouage réalisé par littlekowicz" /> */}
             </div>
 
         </section>
